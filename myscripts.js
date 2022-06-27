1d742bd2ff4292e8395f9b96c883dcdf4cee09aa
KetrinV <92842188+KetrinV@users.noreply.github.com>
2022-06-28 01:11:16 +0300

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,17 @@
+const heading = document.querySelector(".heading")
+heading.textContent = "Tak22"
+console.log(heading)
+
+const texti = document.querySelector(".texti")
+texti.textContent = "you"
+console.log(texti)
+
+const fruits = ["apple", "banana", "strawberry", "plum"]
+fruits.forEach(fruit => [
+    const listItem = document.createElement("li")
+    listItem.classList.add("list-item")
+    listItem = innertText = fruit
+    orderedListElement.appendChild(listItem)
+])
+
+// const orderedListElement = document.querySelector(".component-list")
